9756b42d47744a59fd494ff4797e058e824ee923
Seidazymov Adil <m.kypshakb@gmail.com>
2022-09-14 22:41:01 +0300

    Basic project framework

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+# Импортируем необходимые классы.
+import logging
 
+from telegram.ext import Updater, CommandHandler
+
+from config import App
+
+# Запускаем логгирование
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+
+def start_command(update, context):
+    pass
+
+
+def help_command(update, context):
+    pass
+
+
+def main():
+    updater = Updater(App.config("BOT_TOKEN"))
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start_command))
+    dp.add_handler(CommandHandler("help", help_command))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
